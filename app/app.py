ac80261d842247c5a24e0287e240b4d51e428b8a
gal.lagelpibuxade <ggelpibu7@alumnes.ub.edu>
2025-12-16 12:38:50 +0100

    api finished with frontend

--- app/app.py
+++ app/app.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
 from app.routers.predict_router import router
 
+from fastapi.middleware.cors import CORSMiddleware
+
 app = FastAPI(title="Image Classification API")
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(router)
-
