de6e6581cd35de2f7314d303eadbaec5810cae25
manzife <felipemanzife@gmail.com>
2025-12-16 12:26:35 +0100

    -- fronted

--- app/app.py
+++ app/app.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
 from app.routers import predict_router
 
+from fastapi.middleware.cors import CORSMiddleware
+
 app = FastAPI(title="Image Classification API")
 
-app.include_router(predict_router)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(predict_router.router)
 
